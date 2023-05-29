917265bf329a539853a249b318a7a2ac50889ee6
Dabin <czmcm5@gmail.com>
2023-05-29 17:48:03 +0900

    0529 회원가입 구현
    회원가입 input에 data입력시 데이터 베이스에 저장

--- front/src/pages/Register.jsx
+++ front/src/pages/Register.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Register = () => {
+export const Register = () => {
+  const server = 'http://localhost:3002'
+  
+  // input 값을 변수에 저장
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  // 회원가입 요청
+  const register = () => {
+    // 코드 출처 - https://docs.strapi.io/dev-docs/plugins/users-permissions#registration
+    axios
+    .post(`${ server }/register`, {
+      name: name, // input값을 저장한 변수
+      email: email,
+      password: password,
+    })
+    .then(response => { // then <= 위 코드와 동시작동 안하도록 사용
+      console.log('성공!');
+      console.log('User profile', response.data.user);
+      console.log('User token', response.data.jwt); // 토큰받기
+      // localStorage.setItem('token', response.data.jwt) // 토근 다시 보냄
+      // replace("/"); // 왜? 회원가입 후 바로 로그인 되도록
+    })
+    .catch(error => {
+      console.log('실패했어요:', error.response);
+    });
+  }
+
   const imgUrl = '/images/kakao.svg';
 
   return (
@@ -13,15 +42,35 @@ const Register = () => {
 
       <div className='tab-content'>
         <div className='tab-body'>
+
           <div className='form-element'>
             <p id='subtitle'>닉네임</p>
-            <input id='input' type="text" placeholder='10자 이하' />
+            <input id='input' type="text" placeholder='10자 이하'
+              value={ name }
+              onChange={(event) => {
+                setName(event.target.value); // 이벤트로 일어난 값 => email로 지정
+                // console.log(event.target.value) // input에 입력하고 console에서 테스트
+              }}
+            />
+
             <p id='subtitle'>이메일</p>
-            <input id='input' type="text" placeholder='example@gmail.com' />
+            <input id='input' type="text" placeholder='example@gmail.com'
+              value={ email }
+              onChange={(event) => {
+                setEmail(event.target.value);
+                }}
+              />
+
             <p id='subtitle'>비밀번호</p>
-            <input id='input' type="text" placeholder='7자 이하' />
+            <input id='input' type="password" placeholder='7자 이하'
+              value={ password }
+              onChange={(event) => {
+                setPassword(event.target.value);
+                }}
+            />
           </div>
-          <button id='register-register'>회원가입</button>
+
+          <button onClick={ register } id='register-register'>회원가입</button>
           <button id='register-login'>
             <img src={imgUrl} className='kakao'/>  
             카카오톡으로 회원가입
