dcde55feba829a28766aa7dfc4305fd45454274a
hongsebin <hongsb5837@gmail.com>
2023-05-30 17:52:28 +0900

    0530 Dropdown.jsx Link 추가
    Link로 다른 페이지 연결

--- front/src/components/Dropdown.jsx
+++ front/src/components/Dropdown.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 const Dropdown = () => {
 
     return (
         <div className='dropdown'>
             <ul className='dropdown-ul'>
-                <li className='d1'>🏠</li>
-                <li className='d2'>🧑‍🤝‍🧑</li>
-                <li className='d3'>⚙️</li>
+                <Link to='./'><li className='d1'>🏠</li></Link>
+                <Link to='./friends'><li className='d2'>🧑‍🤝‍🧑</li></Link>
+                <Link to='./mypage'><li className='d3'>⚙️</li></Link>
                 <li className='d4'>🏃🏻</li>
             </ul>
         </div>
