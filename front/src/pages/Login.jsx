945a4f44d82f535dc71d2946eaf9528a38887888
Dabin <czmcm5@gmail.com>
2023-05-30 16:48:25 +0900

    0530 로그인 구현 중
    로그인 시 회원 정보 가져오기 까지 완료

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Login = () => {
+  const server = 'http://localhost:3002'
+  
+  // input 값을 변수에 저장
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // 로그인 요청
+  const login = () => {
+    axios
+    .post(`${ server }/login`, {
+      email: email,
+      password: password,
+    })
+    .then(response => {
+      alert("로그인 요청 성공!");
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.log('실패했어요:', error.response);
+    });
+  }
+
+
+  
   const imgUrl = '/images/kakao.svg';
 
   return (
@@ -13,17 +38,30 @@ const Login = () => {
 
       <div className='tab-content'>
         <div className='tab-body'>
+          
           <div className='form-element'>
             <p id='subtitle'>이메일</p>
-            <input id='input' type="text" placeholder='example@gmail.com' />
+            <input id='input' type="text" placeholder='example@gmail.com'
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+             />
+
             <p id='subtitle'>비밀번호</p>
-            <input id='input' type="text" placeholder='7자 이하' />
+            <input id='input' type="password" placeholder='7자 이하'
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+
           </div>
-          <button id='register-register'>로그인</button>
+
+          <button onClick={ login } id='register-register'>로그인</button>
           <button id='register-login'>
             <img src={imgUrl} className='kakao'/>  
             카카오톡으로 로그인
           </button>
+          
           <Link to='/pwfind' style={{ textDecorationLine: 'none' }}>
             <p id='pwforgot'>비밀번호를 잊으셨나요?</p></Link>
         </div>
